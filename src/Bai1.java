356ed0465c0c274a33444e828829b27f52b1cde5
unknown <duongducchinh160@gmail.com>
2022-07-24 22:30:56 +0700

    TH bai1

---
+++ src/Bai1.java
@@ -0,0 +1,9 @@
+import java.util.Date;
+
+public class Bai1 {
+    public static void main(String[] args){
+        // viết code của bạn ở đây !!!
+        Date date = new Date();// lấy ra thời gian hiện tại
+        System.out.println(date);// in ra thời gian hiện tại
+    }
+}
